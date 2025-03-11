20bd38edc1f11e5891c2e953894faf9f546b91b4
tamannaaaaaaaaa <116890193+tamannaaaaaaaaa@users.noreply.github.com>
2025-03-11 12:48:17 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, This is my edit \n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
